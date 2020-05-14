ba9987f160a73e94e6fcec9a08ec021c2effc214
Gloria Lule <glorialule@protonmail.com>
2020-05-14 12:09:35 -0700

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ADMIN_LULE:NfC2TcpCq6TQyMUK@cluster0-xcfvh.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
